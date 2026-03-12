d2c04822f078aad0663a9e6405a01be3b6b099f3
Kelso Palheta <kelsopalhetadev@gmail.com>
2026-03-12 10:36:43 -0300

    Repara a ordem exata do Middleware CORS no C#

--- src/BatatasFritas.API/Program.cs
+++ src/BatatasFritas.API/Program.cs
@@ -46,8 +46,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowBlazorClient");
 app.UseRouting();
+app.UseCors("AllowBlazorClient");
 app.UseAuthorization();
 app.MapControllers();
 
