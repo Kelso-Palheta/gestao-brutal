bc94245ee5b14fe9783c2290332bee541984bac6
KPM <profkelsopalheta@gmail.com>
2026-05-06 14:43:34 -0300

    fix: migration V018 syntax PostgreSQL

--- src/BatatasFritas.Infrastructure/Migrations/V018__MakeTelefoneClienteNullable.cs
+++ src/BatatasFritas.Infrastructure/Migrations/V018__MakeTelefoneClienteNullable.cs
@@ -7,17 +7,11 @@ public class V018__MakeTelefoneClienteNullable : Migration
 {
     public override void Up()
     {
-        Alter.Table("pedidos")
-            .AlterColumn("telefone_cliente")
-            .AsString(20)
-            .Nullable();
+        Execute.Sql("ALTER TABLE pedidos ALTER COLUMN telefone_cliente DROP NOT NULL;");
     }
 
     public override void Down()
     {
-        Alter.Table("pedidos")
-            .AlterColumn("telefone_cliente")
-            .AsString(20)
-            .NotNullable();
+        Execute.Sql("ALTER TABLE pedidos ALTER COLUMN telefone_cliente SET NOT NULL;");
     }
 }
